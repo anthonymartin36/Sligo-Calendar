0c2af98db8e00f87425de62313e305d3a1af3bd8
anthonymartin36 <anthony.martin36@gmail.com>
2023-11-04 19:55:28 +1300

    getting the calendar Board details

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,60 +1,101 @@
 import express from 'express'
-import { Console } from 'node:console';
+import { Console } from 'node:console'
 
 import fs from 'node:fs/promises'
 //const fs = require('fs')
 //try putting this at the top of the file:
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 //calculate the current date
 const today = new Date()
 let currentDate = {
   cMonth: today.toLocaleString('default', { month: 'long' }),
-  dayOfWeek: today.toLocaleString('default', {weekday: 'long'}),
-  cYear: today.getFullYear(),    
+  dayOfWeek: today.toLocaleString('default', { weekday: 'long' }),
+  cYear: today.getFullYear(),
 }
 //then first line after your route declaration:
-const  __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 //console.log ("directory: " + __dirname  + "   filename:  "  +  __filename  )
 
 const router = express.Router()
 // get Data.json Object
 
 router.get('/', async (req, res) => {
-  
-
   //console.log("Home Directory", path.dirname(__dirname))
   let file = __dirname + '/data.json'
- // console.log('File source : ' + file)
+  // console.log('File source : ' + file)
   const data = await awaitingReadFile(file)
-  //read in the data object 
-  let thisMonth = data.Calendar.find((thisMonth) => thisMonth.id == today.getMonth())
-  currentDate = Object.assign({cTime: today.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1")}, currentDate)
+  //read in the data object
+  let thisMonth = data.Calendar.find(
+    (thisMonth) => thisMonth.id == today.getMonth()
+  )
+  currentDate = Object.assign(
+    { cTime: today.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, '$1') },
+    currentDate
+  )
+
+  // Create an Object  that has an object with in it for every day of the month
+
+  // amend the object to contain each Calendar day of the Month
+  const calendarDays = calendarDayCalc(thisMonth)
+  //console.log(calendarDays)
+
   let viewData = {
     ...currentDate,
-    ...thisMonth
-  } 
-  console.log(viewData)
-  //establish the data month and date to display from to days date.
-  //create an object to return to index to display. 
-  //console.log("Time " + currentDate.time + " / Day : " + currentDate.dayOfWeek + " / Month : " + currentDate.month + " / Year : " + currentDate.year)
+    ...thisMonth,
+    //...calendarDays,
+  }
   res.render('index', viewData)
 })
 
-async function awaitingReadFile(file){
-  let data = ""
-  try{
+async function awaitingReadFile(file) {
+  let data = ''
+  try {
     let json = await fs.readFile(file, 'utf-8')
     data = JSON.parse(json)
     return data
-  }
-  catch(err)
-  {
-    console.log("Error: " + err.message)
+  } catch (err) {
+    console.log('Error: ' + err.message)
   }
   return data
+}
+
+function calendarDayCalc(thisMonth) {
+  const calendarDays = []
+  const notesInDay = thisMonth.notes.filter((note) => note.day != '')
+  //console.log(thisMonth)
+  for (let i = 1; i < thisMonth.days + 1; i++) {
+    // loop through each day in month
+    for (let j = 0; j < notesInDay.length; j++) {
+      // loop through the notes for this month from Data Object, via day in note
+      if (notesInDay[j].day == i) {
+        // if there is a note add it to the array for that day
+        let weekDay = Object.assign(notesInDay[j], {weekday: getWeekDay(i + 1)})
+        calendarDays.push(weekDay)
+      }
+    }
+    if (calendarDays[i - 1] == null) {
+      // ensure on start on double up(i starts and 1)
+      let weekDay = { day: i, weekday: getWeekDay(i+1) }
+      //calendarDays.push(day)
+      calendarDays.push(weekDay)
+    }
+  }
+  console.log(calendarDays)
+  return calendarDays
+}
+
+function getWeekDay(weekDay){
+  var date = new Date(),
+  y = date.getFullYear(),
+  m = date.getMonth()
+  var firstDay = new Date(y, m, weekDay)
+  //console.log(firstDay)
+  //firstDay.getDay()
+
+  return firstDay
 }
 
 export default router
